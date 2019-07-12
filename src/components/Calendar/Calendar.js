f1597cc692bed6b1692f9bbc46e33fc6ea9d295a
Sushasgit <gryshchukoleksandra@gmail.com>
2019-07-13 01:43:38 +0300

    Test flow

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import './Calendar.css';
 import moment from 'moment';
-import './Calendar.css'
 
 export default class Calendar extends Component{
     state = {
@@ -190,23 +189,28 @@ export default class Calendar extends Component{
 
     onDayClick = (e, day ) =>{
         this.props.onDayClick && this.props.onDayClick(e,day);
-
     }
 
-    onDayClick = (e,day) =>{
+    onDayClick = (e,day, week) =>{
         console.log(this.prevMonthQuantityDay())
         console.log(day)
         console.log(this.month())
         console.log(this.year())
         console.log(this.firstDayOfMonth())
         console.log('первый день'+ this.firstDayInNextMonth())
+        console.log('WEEEEk', moment().weekday(day))
+        this.setState({
+            clickedWeek: week,
+            clickedDay: day,
+        })
     }
 
     render() {
         //map weekdays
-        let weekdays = this.weekdaysShort.map((day) =>{
+        let weekdays = this.weekdaysShort.map((day) => {
+
             return (
-                <td key={day} className='week-day'>{day}</td>
+                <div key={day} className='week-day'>{day}</div>
             )
         });
 
@@ -220,9 +224,9 @@ export default class Calendar extends Component{
 
         let daysInNextMonth = [];
         for(let i = 1; i < 7 - (this.firstDayInNextMonth()-1); i++){
-            daysInNextMonth.push(<td key = {i*80} className='empty-slot'>
+            daysInNextMonth.push(<div key = {i*80} className='empty-slot'>
                     {i}
-                </td>
+                </div>
             );
         }
 
@@ -232,10 +236,13 @@ export default class Calendar extends Component{
         let daysInMonth = [];
         for (let d = 1; d <=this.daysInMonth(); d++){
             let className = (d === this.currentDay()? 'day current-day' : 'day');
+            console.log('kmnknmknknk', this.daysInMonth())
+            const weekNum = Math.ceil(d/7) 
+            console.log('weekNum',weekNum)
             daysInMonth.push(
-              <td key={d} className={className}>
-                  <span className="date-number" onClick={(e) => {this.onDayClick(e,d)}}>{d}</span>
-              </td>
+              <div key={d} className={className}>
+                  <span className="date-number" onClick={(e) => {this.onDayClick(e,d, weekNum)}}>{d}</span>
+              </div>
             );
         }
 
@@ -248,8 +255,10 @@ export default class Calendar extends Component{
         totalSlots.forEach((row, i) =>{
             if((i%7)!= 0 || i === 0){
                 cells.push(row);
+                console.log('cellscells', totalSlots)
             }else {
                 let insertRow = cells.slice();
+                console.log('insertRow', insertRow)
                 rows.push(insertRow);
                 cells = [];
                 cells.push(row);
@@ -263,40 +272,48 @@ export default class Calendar extends Component{
 console.log(rows)
         let trElems = rows.map((d,i) =>{
             return (
-                <tr key={i*100} className='days-row'>
-                    {d}
-                </tr>
+                <div className="test">
+                    <div key={i*100} className='week'>
+                        {d}
+                    </div>
+                    {
+                        this.state.clickedWeek === i+1 ? (
+                            <div style={{color: 'deeppink', minHeight: "150px", border: '1px solid black'}}>
+                                Book {this.state.clickedDay}
+                            </div>
+                        ): null
+                    }
+                </div>
             )
         });
 
 
         return(
             <div className="calendar-container">
-                <table className='calendar'>
-                    <thead>
-                        <tr className='calendar-header'>
-                            <td colSpan="5">
+                <div className='calendar'>
+                    <div>
+                        <div className='calendar-header'>
+                            <div colSpan="5">
                                 <this.MonthNav />
                                 {" "}
                                 <this.YearNav />
-                                <td colSpan="5" className="nav-month">
+                                <div colSpan="5" className="nav-month">
                                     <i className="prev fa fa-fw fa chevron left"
                                        onClick={(e) =>{this.prevMonth()}}>
                                     </i>
                                     <i className="prev fa fa-fw fa chevron right"
                                        onClick={(e) =>{this.nextMonth()}}>
                                     </i>
-                                </td>
-                            </td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr className="week-day-row">
-                            {weekdays}
-                        </tr>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        {weekdays}
+                        
                         {trElems}
-                    </tbody>
-                </table>
+                    </div>
+                </div>
             </div>
         );
     }
