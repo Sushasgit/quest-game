bfbeefa5d05c9d21968ab6ff360f53ffe879f468
Andrey Nemodruk <ikibana2@gmail.com>
2019-07-13 00:28:46 +0300

    added calendar

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import './App.css';
 import Banner from './components/Banner/Banner';
 import Advantages from './components/Advantages/Advantages';
 import Parallax from './components/Parallax';
-
+import Calendar from './components/Calendar/Calendar';
 const App = () => {
   const layersGroup = [<Banner />, <Advantages />];
   return (
     <div className="app">
       <Parallax layersGroup={layersGroup} />
+        <Calendar onDayClick = {(e,day) => this.onDayClick(e,day)}/>
     </div>
   );
 };
