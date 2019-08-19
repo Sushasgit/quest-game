26c46709837474036a65efced41a90e36cec69fe
Andrey Nemodruk <ikibana2@gmail.com>
2019-08-19 14:35:30 +0300

    added style block booking

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -233,7 +233,7 @@ class Calendar extends Component {
                   this.navigationMonth('toCurrentMonth');
                 }}
               >
-                  Вернуться в
+                  Back to
                 {' '}
                 {' '}
                 {currentMonth}
@@ -283,19 +283,19 @@ class Calendar extends Component {
                           <ul>
                             {
                               CALENDAR_ORDER_LIST.map((item, i) => (
-                                <li>
-                                  <span>{item.time}</span>
+                                <li className="li_item_time">
+                                  <span className="item_time">{item.time}</span>
                                   {item.available
                                     ? (
-                                      <div>
-                                        <span>Это время доступно для резервирования</span>
-                                        <button>Зарезервировать время</button>
+                                      <div className="book_item_time">
+                                        <span className="available_item_time">доступно для резервирования</span>
+                                        <button className="button_book">Зарезервировать</button>
                                       </div>
                                     )
                                     : (
-                                      <div>
-                                        <span>Это время уже зарезервироввано</span>
-                                        <button>Недоступно</button>
+                                      <div className="book_item_time">
+                                        <span className="available_item_time">уже зарезервировано</span>
+                                        <button disabled="disabled" className="button_book disable">Недоступно</button>
                                       </div>
                                     )}
                                 </li>
